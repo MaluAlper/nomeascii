848e212aa27c405266f53abe15fa9433c6546938
Maria Luiza <malualper@hotmail.com>
2020-06-04 23:19:47 -0300

    nomeascii

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Figgle;
+
+namespace nomeascii
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("figgle: Meu Nome");
+            Console.WriteLine(FiggleFonts.Standard.Render("MARIA LUIZA ALVES PEREIRA"));
+
+        }
+    }
+}
